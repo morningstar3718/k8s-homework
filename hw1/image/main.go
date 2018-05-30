1930779ec3d381a9139bf581172995c87e60eeaf
morningstar3718 <morningstar3718@gmail.com>
2018-05-30 17:39:18 +0800

    修正 golang server main.go tab

--- hw1/image/main.go
+++ hw1/image/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 )
 
 type Hello struct{}
 
 func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello World</h1>")
+    fmt.Fprint(w, "<h1>Hello World</h1>")
 }
 
 func main() {
-	var h Hello
-	http.ListenAndServe(":5566", h)
+    var h Hello
+    http.ListenAndServe(":5566", h)
 }
 
